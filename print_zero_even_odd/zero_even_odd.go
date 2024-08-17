401fe9d012e8e1b848ac333e22b5934887c03936
skookumchoocher <natebrooks004@gmail.com>
2024-08-17 11:13:50 -0500

    zeroevenodd don't close channels

--- print_zero_even_odd/zero_even_odd.go
+++ print_zero_even_odd/zero_even_odd.go
@@ -7,16 +7,16 @@ type ZeroEvenOdd struct {
 	odd  chan struct{}
 }
 
-func NewZeroEvenOdd(n int) *ZeroEvenOdd {
-	z := &ZeroEvenOdd{n: n}
+func NewZeroEvenOdd(n int) ZeroEvenOdd {
+	z := ZeroEvenOdd{n: n}
 	z.zero = make(chan struct{}, 1)
 	z.even = make(chan struct{})
 	z.odd = make(chan struct{})
-	z.zero <- struct{}{}
 	return z
 }
 
 func (z *ZeroEvenOdd) Zero(printNumber func(int)) {
+	z.zero <- struct{}{}
 	for i := 1; i <= z.n; i++ {
 		<-z.zero
 		printNumber(0)
@@ -26,27 +26,20 @@ func (z *ZeroEvenOdd) Zero(printNumber func(int)) {
 			z.even <- struct{}{}
 		}
 	}
-	close(z.zero)
 }
 
 func (z *ZeroEvenOdd) Even(printNumber func(int)) {
 	for i := 2; i <= z.n; i += 2 {
 		<-z.even
 		printNumber(i)
-		if i < z.n {
-			z.zero <- struct{}{}
-		}
+		z.zero <- struct{}{}
 	}
-	close(z.even)
 }
 
 func (z *ZeroEvenOdd) Odd(printNumber func(int)) {
 	for i := 1; i <= z.n; i += 2 {
 		<-z.odd
 		printNumber(i)
-		if i < z.n {
-			z.zero <- struct{}{}
-		}
+		z.zero <- struct{}{}
 	}
-	close(z.odd)
 }
